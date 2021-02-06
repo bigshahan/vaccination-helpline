562e7e8ba255d4a7d2563481871dcae7ff2d931d
Shahan Khan <contact@shahan.me>
2021-02-05 19:52:21 -0800

    Xstate + some basic session management

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
+const { urlencoded } = require('body-parser');
 const express = require('express');
 const config = require('./config');
+const { vaccineMachine, getStateForSession, saveStateForSession } = require('./vaccineMachine');
 
 const app = express();
-const PORT = config.PORT;
-const AIRTABLE_API_KEY = config.AIRTABLE_API_KEY;
+app.use(urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.send('💉 🌎'));
+const PORT = config.PORT;
+
+app.get('/', (req, res) => res.send('💉🌎🌍🌏'));
 
 app.post('/v1/twilio/hook', (req, res) => {
+  console.log(req.body);
+
+  const {
+    CallSID, From,
+  } = req.body;
+
+  const currentState = getStateForSession(CallSID);
+
+  /*
     const twiml = new VoiceResponse();
     twiml.say({voice: 'alice'}, 'Please leave your name and phone number at the beep.');
+*/
 
+  // const initialState = vaccineMachine.getInitialState();;
+  // console.log(initialState.value);
+
+  console.log(`State before machine is run: ${currentState.value}`);
+
+  const nextState = vaccineMachine.transition(currentState, {res: res, type: 'PRESS_ONE'});
+
+  saveStateForSession(CallSID, nextState);
+
+  console.log(`After state is run: ${currentState.value}`);
 
 
     res.setHeader('Content-Type', 'text/xml');
-    res.write(twiml.toString());
+    // res.write(twiml.toString());
     res.end();
 });
 
---
+++ vaccineMachine.js
@@ -0,0 +1,63 @@
+const Machine = require('xstate').Machine;
+
+const vaccineMachine = Machine({
+    id: 'vaccine',
+    initial: 'welcome',
+    context: {
+      retries: 0
+    },
+    states: {
+      welcome: {
+        on: {
+          PRESS_ONE: 'voicemail',
+          PRESS_TWO: 'numberInput',
+          PRESS_THREE: 'welcome',
+        },
+        actions: ['welcome'],
+      },
+      numberInput: {
+        on: {
+          PRESS_POUND: 'numberInputConfirm',
+        }
+      },
+      numberInputConfirm: {
+        on: {
+          PRESS_ONE: 'voicemail',
+          PRESS_TWO: 'numberInput',
+          PRESS_THREE: 'numberInputConfirm',
+        }
+      },
+      voicemail: {
+        on: {
+          PRESS_POUND: 'hangup',
+        }
+      },
+      hangup: {
+        type: 'final',
+      }
+    }
+  }, {
+    actions: {
+      welcome: (context, event) => {
+        context.res.write('<Say>Hello</Say>');
+        context.res.send();
+        console.log('Welcome to Vaccination Helpline. Please enter your phone number at the beep');
+      }
+    }
+  });
+
+const existingSessions = {};
+
+const getStateForSession = (sid) => {
+  return existingSessions[sid] || vaccineMachine.initialState;
+}
+
+const saveStateForSession = (sid, newState) => {
+  existingSessions[sid] = newState;
+}
+
+module.exports = {
+  vaccineMachine,
+  getStateForSession,
+  saveStateForSession,
+}
