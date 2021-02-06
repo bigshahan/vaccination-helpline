abe127c9c4404059c10f11bb6f28fefcb4ae4262
Shahan Khan <contact@shahan.me>
2021-02-05 19:00:25 -0800

    Basic twilio

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
 const express = require('express');
 const config = require('./config');
-
-console.log(config)
 
 const app = express();
 const PORT = config.PORT;
@@ -9,7 +9,16 @@ const AIRTABLE_API_KEY = config.AIRTABLE_API_KEY;
 
 app.get('/', (req, res) => res.send('💉 🌎'));
 
-app.post('/v1/twilio/hook', (req, res) => res.send('🧟‍♀️'));
+app.post('/v1/twilio/hook', (req, res) => {
+    const twiml = new VoiceResponse();
+    twiml.say({voice: 'alice'}, 'Please leave your name and phone number at the beep.');
+
+
+
+    res.setHeader('Content-Type', 'text/xml');
+    res.write(twiml.toString());
+    res.end();
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
