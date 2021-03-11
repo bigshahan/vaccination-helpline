25816adef1f6c899b957e13747ff3853b2d21f32
Yefim Vedernikoff <hi@yef.im>
2021-03-10 23:18:52 -0700

    JSON.stringify

--- twilioHook.js
+++ twilioHook.js
@@ -88,7 +88,7 @@ const twilioHook = (req, res) => {
               url: RecordingUrl,
             }],
             'Zip': state.context.zipCode,
-            'Called At': state.context.createdAt,
+            'Called At': JSON.stringify(state.context.createdAt),
           },
         },
       ])
